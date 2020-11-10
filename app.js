6b0753f3a576f9c07a5e128630e6ff725f66e018
AlexisRoe <A.Roehrling@gmx.de>
2020-11-10 14:05:31 +0100

    Add loop for 3 tries

--- app.js
+++ app.js
@@ -4,15 +4,26 @@ const chalk = require('chalk');
 // reading arguments from console
 const args = process.argv.slice(2);
 
-// 1. standard node version
-// interactive communication with commandline
-// const readline = require("readline").createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// })
+// old fashion inquirer version
+// inquirer.prompt(questions).then((answers) => {
+//     if (answers.masterPwd === 'masterpwd') {
+//         console.log(chalk.green(`Your Masterpassword is CORRECT`));
+//         inquirer.prompt(pwdQuestion).then((answers) => {
+//             if (pwdSafe[answers.pwdQuery]) {
+//                 console.log(`Password found: ${pwdSafe[answers.pwdQuery]}`);
+//             } else {
+//                 console.log(chalk.yellow('Password not found.'));
+//             }
+//         });
+//     } else {
+//         console.log(chalk.red(`Your Masterpassword is WRONG`));
+//         process.exit();
+//     }
+// });
 
-// 2. inquirer version
-console.log(`*** Password Manager 0.0.2 ***`);
+// 4. Promise in modern JS - await/async
+
+// data storage
 
 const questions = [
     {
@@ -27,29 +38,50 @@ const pwdQuestion = [
     {
         type: 'string',
         name: 'pwdQuery',
-        message: 'Whats next, pussycat?\n>',
+        message: `Whats next, pussycat?\n>`,
     },
 ];
 
 const pwdSafe = {
+    master: 'Maximus',
     wifi: '123456',
     puk: 'gambler',
     home: 'nextTime',
 };
 
-// 3. colorize output in console
-inquirer.prompt(questions).then((answers) => {
-    if (answers.masterPwd === 'Maximus') {
-        console.log(chalk.green(`Your Masterpassword is CORRECT`));
-        inquirer.prompt(pwdQuestion).then((answers) => {
-            if (pwdSafe[answers.pwdQuery]) {
-                console.log(`Password found: ${pwdSafe[answers.pwdQuery]}`);
-            } else {
-                console.log(chalk.yellow('Password not found.'));
-            }
-        });
-    } else {
+
+//  helper functions
+async function validateAccess() {
+    const { masterPwd } = await inquirer.prompt(questions);
+
+    if (pwdSafe.master !== masterPwd) {
         console.log(chalk.red(`Your Masterpassword is WRONG`));
         process.exit();
     }
-});
+}
+
+async function findPassword(rounds) {
+    const { pwdQuery } = await inquirer.prompt(pwdQuestion);
+
+    if (!pwdSafe[pwdQuery]) {
+        console.log(chalk.yellow(`Password not found (${rounds - 1} tries left)`));
+        rounds = rounds -1;
+        return rounds;
+    }
+
+    console.log(chalk.green(`Password found: ${pwdSafe[pwdQuery]}`));
+    process.exit();
+}
+
+// actual program code
+
+async function app() {
+    console.log(`*** Password Manager 0.0.2 ***`);
+    await validateAccess();
+    let rounds = 3;
+    while (rounds > 0) {
+        rounds = await findPassword(rounds);
+    }
+}
+
+app();
