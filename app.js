26dbcb035764889deb6fba2769c6d4beb4755397
AlexisRoe <A.Roehrling@gmx.de>
2020-11-10 16:54:35 +0100

    Clean up commented code

--- app.js
+++ app.js
@@ -2,34 +2,6 @@ const inquirer = require('inquirer');
 const chalk = require('chalk');
 const fs = require('fs');
 
-/*
- * old fashion inquirer version
- */
-
-// inquirer.prompt(questions).then((answers) => {
-//     if (answers.masterPwd === 'masterpwd') {
-//         console.log(chalk.green(`Your Masterpassword is CORRECT`));
-//         inquirer.prompt(pwdQuestion).then((answers) => {
-//             if (pwdSafe[answers.pwdQuery]) {
-//                 console.log(`Password found: ${pwdSafe[answers.pwdQuery]}`);
-//             } else {
-//                 console.log(chalk.yellow('Password not found.'));
-//             }
-//         });
-//     } else {
-//         console.log(chalk.red(`Your Masterpassword is WRONG`));
-//         process.exit();
-//     }
-// });
-
-/*
- * Promise in modern JS - await/async
- */
-
-// reading arguments from console
-// const args = process.argv.slice(2);
-
-//  helper functions
 async function validateAccess(master) {
     const { masterPwd } = await inquirer.prompt([
         {
@@ -88,32 +60,6 @@ async function showPasswords(category) {
     console.log(chalk.green(`name: ${category[index].name}\nkey: ${category[index].key}`));
 }
 
-// finding passwords with rounds
-// async function findPassword(rounds, safe) {
-
-//     if (rounds <= 0) {
-//         return;
-//     }
-
-//     const { pwdQuery } = await inquirer.prompt([
-//         {
-//             type: 'string',
-//             name: 'pwdQuery',
-//             message: `What password are you looking for?\n>`,
-//         },
-//     ]);
-
-//     if (!safe[pwdQuery]) {
-//         console.log(chalk.yellow(`Password not found (${rounds - 1} tries left)`));
-//         rounds--;
-//         await findPassword(rounds, safe);
-//         return;
-//     }
-
-//     console.log(chalk.green(`Password found: ${safe[pwdQuery]}`));
-// }
-
-// actual program code
 
 async function app() {
     console.log(`*** Password Manager 0.0.2 ***`);
@@ -136,5 +82,5 @@ async function app() {
     process.exit();
 }
 
-// Lets GO
+
 app();
