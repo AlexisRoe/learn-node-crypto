66714228aed23b9c12f1180c1d324475591a0367
AlexisRoe <A.Roehrling@gmx.de>
2020-11-10 12:33:07 +0100

    Cleanup & create package.json

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const inquirer = require('inquirer');
 const chalk = require('chalk');
-const { stringify } = require('querystring');
 
 // reading arguments from console
 const args = process.argv.slice(2);
-// console.log(args);
 
 // 1. standard node version
 // interactive communication with commandline
@@ -13,41 +11,31 @@ const args = process.argv.slice(2);
 //     output: process.stdout
 // })
 
-// // Outputs the question and wait for the answer to display
-// readline.question(`What´s your masterpassword\n> `, master => {
-//     console.log(`Your Masterpassword is: ${master}`);
-//     readline.close();
-// })
-
 // 2. inquirer version
-
-if (args[0]) {
-    console.log(`*** Password Manager ***\n *** Hello ${args[0]} ***`);
-} else {
-    console.log(`*** Password Manager ***\n *** Hello Stranger ***`)
-}
+console.log(`*** Password Manager 0.0.2 ***`);
 
 const questions = [
     {
         type: 'password',
         name: 'masterPwd',
-        message: 'Whats your Masterpassword?\n>',
+        message: '> Password\n>',
+        mask: true,
     },
 ];
 
 const pwdQuestion = [
     {
-        type: "string",
-        name: "pwdQuery",
-        message: "Whats next, pussycat?\n>"
-    }
-]
+        type: 'string',
+        name: 'pwdQuery',
+        message: 'Whats next, pussycat?\n>',
+    },
+];
 
 const pwdSafe = {
-    wifi: "123456",
-    puk: "gambler",
-    home: "nextTime"
-}
+    wifi: '123456',
+    puk: 'gambler',
+    home: 'nextTime',
+};
 
 // 3. colorize output in console
 inquirer.prompt(questions).then((answers) => {
@@ -55,9 +43,9 @@ inquirer.prompt(questions).then((answers) => {
         console.log(chalk.green(`Your Masterpassword is CORRECT`));
         inquirer.prompt(pwdQuestion).then((answers) => {
             if (pwdSafe[answers.pwdQuery]) {
-                console.log(`Password found: ${pwdSafe[answers.pwdQuery]}`)
+                console.log(`Password found: ${pwdSafe[answers.pwdQuery]}`);
             } else {
-                console.log("Password not found.")
+                console.log(chalk.yellow('Password not found.'));
             }
         });
     } else {
