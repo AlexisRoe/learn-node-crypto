e21d4d53cd609a9effa7256c818646350eb91419
AlexisRoe <A.Roehrling@gmx.de>
2020-11-11 11:03:52 +0100

    Refactore code in seperate moduls

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const inquirer = require('inquirer');
 const chalk = require('chalk');
-const fs = require('fs');
+const fs = require('fs').promises;
 
-async function validateAccess(master) {
-    const { masterPwd } = await inquirer.prompt([
+// entry control
+async function isValidateAccess(master) {
+    const { pwdInput } = await inquirer.prompt([
         {
             type: 'password',
-            name: 'masterPwd',
+            name: 'pwdInput',
             message: chalk.yellow('Masterpassword?\n'),
             mask: true,
         },
     ]);
 
-    if (master !== masterPwd) {
+    if (master !== pwdInput) {
         console.log(chalk.red(`Your Masterpassword is WRONG`));
         return false;
     }
@@ -20,66 +21,128 @@ async function validateAccess(master) {
     return true;
 }
 
-async function chooseCategory(safe) {
+// show passwords in safe
+async function getUserChoice(listChoices, topic) {
     const choices = [];
 
-    for (const key in safe) {
+    for (const key in listChoices) {
         choices.push(key);
     }
 
+    const { choice } = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choice',
+            message: chalk.yellow(`${topic} : Choose a property`),
+            choices: choices,
+        },
+    ]);
+
+    return choice;
+}
+
+async function showPasswordSafe(safe) {
+    const category = await getUserChoice(safe, 'Category');
+    const password = await getUserChoice(safe[category], 'Passwords');
+
+    console.log(chalk.green(`Your result:`));
+    console.log(`name: ${category}\nkey: ${safe[category][password]}\n`);
+}
+
+// Writing & Reading File Functions
+async function getData() {
+    const promise = await fs.readFile('./db.json', 'utf8');
+    const data = JSON.parse(promise);
+    return data;
+}
+
+async function setData(data) {
+    const dataToSend = JSON.stringify(data, null, 2);
+    await fs.writeFile('./db.json', dataToSend);
+    return data;
+}
+
+// creating new key/ value pairs
+async function setPwd(safe) {
     const { category } = await inquirer.prompt([
         {
-            type: 'list',
+            type: 'string',
             name: 'category',
-            message: chalk.yellow('Choose a category'),
-            choices: choices,
+            message: chalk.yellow(`Storing data in which category?\n`),
         },
     ]);
 
-    return safe[category];
-}
-
-async function showPasswords(category) {
-    const choices = [];
-
-    category.forEach((element) => {
-        choices.push(element.name);
-    });
-
-    const { key } = await inquirer.prompt([
+    const { keyName } = await inquirer.prompt([
         {
-            type: 'list',
-            name: 'key',
-            message: chalk.yellow(`Choose a property`),
-            choices: choices,
+            type: 'string',
+            name: 'keyName',
+            message: chalk.yellow(`What is the key/name to be stored?\n`),
         },
     ]);
 
-    const index = choices.findIndex((element) => element === key);
+    const { keyValue } = await inquirer.prompt([
+        {
+            type: 'string',
+            name: 'keyValue',
+            message: chalk.yellow(`What is the password to be stored?\n`),
+        },
+    ]);
 
-    console.log(chalk.green(`name: ${category[index].name}\nkey: ${category[index].key}`));
+    const { public: data } = safe;
+
+    if (!data[category]) {
+        data[category] = {};
+    }
+
+    if (keyValue && category && keyName) {
+        data[category][keyName] = keyValue;
+
+        await setData(safe);
+        console.log(chalk.green(`given values set accepted`));
+        return;
+    }
+
+    console.log(chalk.red('Something goes horrible wrong\n'));
 }
 
+// display existing key/ value pairs
 
-async function app() {
-    console.log(`*** Password Manager 0.0.2 ***`);
-
-    const master = '0000';
-
+async function getPwd(data) {
     try {
-        if (await validateAccess(master)) {
-            const pwd = await JSON.parse(fs.readFileSync('./db.json', 'utf8'));
-            const category = await chooseCategory(pwd);
-            await showPasswords(category);
-        }
+        const category = await chooseCategory(data);
+        await showPasswords(category);
     } catch (err) {
         console.log(err);
         return;
+    }
+
+    return;
+}
+
+// ***********************
+// the actual app.js ;-)
+// ***********************
+
+async function run() {
+    console.log(`*** Password Manager 0.0.2 ***`);
+
+    const args = process.argv.slice(2);
+    const safe = await getData();
+    const { master, public: data } = safe;
+
+    if (await isValidateAccess(master)) {
+        if (args[0] === 'set' || args[0] === '-s') {
+            await setPwd(safe);
+        } else if (args[0] === 'get' || args[0] === '-g') {
+            // await getPwd(data);
+        } else {
+            // await getPwd(data);
+            await showPasswordSafe(data);
+        }
     }
 
     console.log(`*** Good Bye ****`);
     process.exit();
 }
 
-
-app();
+run();
