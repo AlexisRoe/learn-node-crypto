5db6b7de78ab4853056ebf25c25ccbd78d159a52
AlexisRoe <A.Roehrling@gmx.de>
2020-11-11 11:44:58 +0100

    Refactore file handler

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-const inquirer = require('inquirer');
-const chalk = require('chalk');
-
 const { getData, setData } = require("./lib/filehandler");
 const { isValidateAccess } = require("./lib/validateaccess");
 const { showPasswordSafe } = require("./lib/showsafe");
@@ -10,16 +7,16 @@ const { createNewSet } = require("./lib/createset");
 async function run() {
     console.log(`*** Password Manager 0.0.2 ***`);
 
+    const file = './db.json';
+
     const args = process.argv.slice(2);
-    const safe = await getData();
+    const safe = await getData(file);
     const { master, public: data } = safe;
 
     if (await isValidateAccess(master)) {
         if (args[0] === 'set' || args[0] === '-s') {
             const newSet = await createNewSet(safe);
-            await setData(newSet);
-        } else if (args[0] === 'get' || args[0] === '-g') {
-            await showPasswordSafe(data);
+            await setData(file, newSet);
         } else {
             await showPasswordSafe(data);
         }
