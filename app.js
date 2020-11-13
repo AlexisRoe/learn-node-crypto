a61d829a0ba1ca37a2ebedd78ff070ef246d56f2
AlexisRoe <A.Roehrling@gmx.de>
2020-11-13 09:46:42 +0100

    Install and config dotenv envirement variables

--- app.js
+++ app.js
@@ -3,30 +3,83 @@ const { isValidateAccess } = require('./lib/validateaccess');
 const { showPasswordSafe } = require('./lib/showsafe');
 const { createNewSet } = require('./lib/createset');
 const { validateParams } = require('./lib/validateparams');
+const chalk = require('chalk');
+require('dotenv').config()
 
 // MongoDB
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
 // Connection URL
-const url =
-    'mongodb+srv://alexis:FQYi8E2EKNiznjJr@cluster0.nqjuk.mongodb.net/learn-crypto?retryWrites=true';
+const url = process.env.DB_URL
 
 // Use connect method to connect to the Server
 MongoClient.connect(url, function (err, client) {
     assert.equal(null, err);
     const db = client.db('test');
 
-    db.collection('inventory')
-        .insertOne({
-            item: 'canvas',
-            qty: 100,
-            tags: ['cotton'],
-            size: { h: 28, w: 35.5, uom: 'cm' },
-        })
-        .then(function (result) {
-            // console.log(result);
-        });
+    // insert data into database
+
+    // db.collection('inventory')
+    //     .insertOne({
+    //         item: 'canvas',
+    //         qty: 100,
+    //         tags: ['cotton'],
+    //         size: { h: 28, w: 35.5, uom: 'cm' },
+    //     })
+    //     .then(function (result) {
+    //         // console.log(result);
+    //     });
+
+    // read all data from database
+
+    // var cursor = db.collection('inventory').find({});
+    // function iterateFunc(doc) {
+    //     console.log(JSON.stringify(doc, null, 4));
+    //  }
+    //  function errorFunc(error) {
+    //     console.log(error);
+    //  }
+    //  cursor.forEach(iterateFunc, errorFunc);
+
+    // insert more data at once
+
+    // db.collection('inventory').insertMany([
+    //     { item: "journal",
+    //       qty: 25,
+    //       size: { h: 14, w: 21, uom: "cm" },
+    //       status: "A"},
+    //     { item: "notebook",
+    //       qty: 50,
+    //       size: { h: 8.5, w: 11, uom: "in" },
+    //       status: "A"},
+    //     { item: "paper",
+    //       qty: 100,
+    //       size: { h: 8.5, w: 11, uom: "in" },
+    //       status: "D"},
+    //     { item: "planner",
+    //       qty: 75, size: { h: 22.85, w: 30, uom: "cm" },
+    //       status: "D"},
+    //     { item: "postcard",
+    //       qty: 45,
+    //       size: { h: 10, w: 15.25, uom: "cm" },
+    //       status: "A"}
+    //   ])
+    //   .then(function(result) {
+    //     // process result
+    //   })
+
+    const cursor = db.collection('inventory').find({ size: { h: 14, w: 21, uom: 'cm' } });
+
+    function iterateFunc(doc) {
+        console.log(JSON.stringify(doc, null, 4));
+    }
+
+    function errorFunc(error) {
+        console.log(error);
+    }
+
+    cursor.forEach(iterateFunc, errorFunc);
 
     client.close();
 });
@@ -37,7 +90,7 @@ async function run() {
     const file = './db.json';
     const instructions = await validateParams(process.argv);
 
-    const master = await readMasterPwd();
+    const master = process.env.MASTER_PWD
 
     if (await isValidateAccess(master)) {
         const safe = await getData(file);
