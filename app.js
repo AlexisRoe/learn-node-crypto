8aab3cff50f5b7c183db0f2b33f562b4262ca1de
AlexisRoe <A.Roehrling@gmx.de>
2020-11-15 22:42:11 +0100

    Install all functions and refactore it

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const chalk = require('chalk');
 
+const { askforPasswordtoChoose } = require('./components/passwords');
 const { showCategories } = require('./components/category');
 const { showMainMenu } = require('./components/mainmenu');
-
+const { createPasswordList } = require('./lib/createlists');
 const { isValidateAccess } = require('./lib/validation');
-const { showOptions, askUser, choosePassword } = require('./lib/askuser');
+const { showOptions, askUser } = require('./lib/askuser');
 const { encrypt, decryptPwd } = require('./lib/crypto');
 const {
     connect,
@@ -14,103 +15,67 @@ const {
     insertNewDocument,
     changeDocument,
     deleteDocument,
+    aggregate,
 } = require('./lib/database');
-
-// async function old() {
-
-//         const existingCategories = await find(process.env.DB_COLLECTION, {
-//             category: { $exists: true },
-//         });
-//         const categories = createCategoryList(existingCategories);
-
-//         if (instructions.write) {
-//             const passwordID = await choosePassword(categories);
-//             const newPassword = await askUser(`Whats the new Password?\n`);
-//             const encryptedPassword = encrypt(newPassword, master);
-//             await changeDocument(process.env.DB_COLLECTION, passwordID, encryptedPassword);
-
-//             console.log(chalk.green(`Password changed`));
-//         } else if (instructions.read) {
-//             const passwordID = await choosePassword(categories);
-//             const passwordDocument = await find(process.env.DB_COLLECTION, {
-//                 _id: passwordID,
-//             });
-
-//             console.log(
-//                 `name: ${passwordDocument[0].name}\nkey: ${decryptPwd(
-//                     passwordDocument[0].value,
-//                     master
-//                 )}\n`
-//             );
-//         } else if (instructions.new) {
-//             const expandCategories = [...categories, '+ New Category'];
-//             const choosenCategory = await showOptions(
-//                 expandCategories,
-//                 `Choose a category from below for continuing`
-//             );
-
-//             const newDocument = {
-//                 category: choosenCategory,
-//             };
-
-//             if (choosenCategory === '+ New Category') {
-//                 const newCategory = await askUser(`Whats the name of the new category?\n`);
-//                 newDocument.category = newCategory;
-//             }
-
-//             newDocument.name = await askUser(`What is the key/name to be stored?\n`);
-//             const rawValue = await askUser(`What is the password to be stored?\n`);
-//             newDocument.value = encrypt(rawValue, master);
-
-//             await insertNewDocument(process.env.DB_COLLECTION, newDocument);
-//             console.log(chalk.green(`new Passwort successfully stored`));
-//         } else if (instructions.delete) {
-//             const passwordID = await choosePassword(categories);
-//             await deleteDocument(process.env.DB_COLLECTION, { _id: passwordID });
-//             console.log(chalk.green(`Password deleted`));
-//         }
-//     } else {
-//         console.log(chalk.red(`Your Masterpassword is WRONG`));
-//     }
-
-//     console.log(`*** Good Bye ****`);
-//     closeConnection();
-//     process.exit();
-// }
 
 async function passwordSafe() {
     const instruction = await showMainMenu();
 
     if (instruction.exit) {
         return;
-    } 
+    }
 
     console.log(chalk.grey('Connecting to database ...'));
     await connect(process.env.DB_URL, process.env.DB_NAME);
 
     if (instruction.search) {
         // nothing here right now
+        const query = '/wif/';
+        const documents = await find(
+            process.env.DB_COLLECTION,
+            { name: { $regex: query, $option: 'ig' } },
+        );
+        console.log(documents);
     } else if (instruction.read) {
-        const category = await showCategories(false);
-        console.log(category);
+        const passwordID = await askforPasswordtoChoose('Choose a password to display');
+        const queryPwd = [{ $match: { _id: passwordID } }, { $project: { category: false } }];
+        const passwordDocument = await aggregate(process.env.DB_COLLECTION, queryPwd);
+        console.log(
+            chalk.green(`name: ${passwordDocument[0].name}\nkey: ${decryptPwd(
+                passwordDocument[0].value,
+                process.env.MASTER_PWD
+            )}`)
+        );
+        
     } else if (instruction.write) {
-        const category = await showCategories(false);
+        const passwordID = await askforPasswordtoChoose('Choose a password to change');
+        const newPassword = await askUser(`Whats the new Password?\n`);
+        const encryptedPassword = encrypt(newPassword, process.env.MASTER_PWD);
+        await changeDocument(process.env.DB_COLLECTION, passwordID, encryptedPassword);
+        console.log(chalk.green(`Password changed`));
     } else if (instruction.new) {
-        const category = await showCategories(true);
+        const newDocument = {};
+        newDocument.category = await showCategories(true);
+        newDocument.name = await askUser(`What is the key/name to be stored?\n`);
+        const rawValue = await askUser(`What is the password to be stored?\n`);
+        newDocument.value = encrypt(rawValue, process.env.MASTER_PWD);
+        await insertNewDocument(process.env.DB_COLLECTION, newDocument);
+        console.log(chalk.green(`new Passwort successfully stored`));
     } else if (instruction.delete) {
-        const category = await showCategories(false);
+        const passwordID = await askforPasswordtoChoose('Choose a password to delete');
+        await deleteDocument(process.env.DB_COLLECTION, { _id: passwordID });
+        console.log(chalk.green(`Password deleted`));
     }
 
     console.log(chalk.grey('Cut the database loose ...'));
     closeConnection();
-    passwordSafe();
-
+    await passwordSafe();
 }
 
 async function run() {
     console.log(`*** Password Manager 0.0.2 ***`);
-    const master = process.env.MASTER_PWD;
-    if (await isValidateAccess(master)) {
+
+    if (await isValidateAccess(process.env.MASTER_PWD)) {
         await passwordSafe();
     } else {
         console.log(chalk.red(`Your Masterpassword is WRONG`));
